2a2559c931cc2314f1259606e14f37e4a783cf20
eugene.tolmachev <eugene.tolmachev@prolucid.ca>
2015-08-12 13:47:56 -0400

    starting gh-pages

--- src/FstGuaranteed/Submit.fsx
+++
@@ -1,6 +0,0 @@
-﻿#I "../../Refs"
-#load "StormSubmit.fsx"
-
-let binDir = "src/FstGuaranteed/bin/Debug"
-
-StormSubmit.runTopology binDir "localhost" StormSubmit.default_nimbus_port
--- src/FstSample/Submit.fsx
+++
@@ -1,6 +0,0 @@
-﻿#I "../../Refs"
-#load "StormSubmit.fsx"
-
-let binDir = "build"
-
-StormSubmit.runTopology binDir "localhost" StormSubmit.default_nimbus_port
